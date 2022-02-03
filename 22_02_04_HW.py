62d6dac44a7cbba8c1b3ec614b2617583f776821
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2022-02-03 21:09:56 +0100

    For loop

---
+++ 22_02_04_HW.py
@@ -0,0 +1,42 @@
+
+# Task 112203
+
+n = int(input("Please enter a number "))
+sum = 0
+for i in range(1, n+1):
+    sum+= i
+print(sum)
+
+# Task 3528
+
+a = int(input("Please enter a number "))
+b = int(input("Please enter a number "))
+for i in range(a, b+1):
+    print(i, end=' ')
+
+# Task 3532
+
+n = int(input("Please enter a number "))
+sumCubed = 0
+for i in range i**3
+print(sumCubed)
+
+# Task 3538
+
+a = int(input("Please enter a number "))
+b = int(input("Please enter a number "))
+for i in range(a + a%2, b+1, 2):
+    print(i, end=' ')
+
+# Task 3541
+
+n = int(input("Please enter a quantity of numbers "))
+count = 0
+for i in range(n):
+    a = int(input())
+    if a == 0:
+        count += 1
+print(count)
+
+
+
