6ff81d6abf17978373418f8ead7c44cea8cc1359
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2024-02-11 23:45:35 +0100

    change locators, add negative test

--- Py_Selenium/unittest_3_2_13.py
+++ Py_Selenium/unittest_3_2_13.py
@@ -15,11 +15,11 @@ class TestRegistration(unittest.TestCase):
         driver.get(link)
 
         # fill in obligatory fields
-        first_name = driver.find_element(By.CSS_SELECTOR, '.first_block div:nth-child(1) input')
+        first_name = driver.find_element(By.CSS_SELECTOR, '.first_block .first_class input')
         first_name.send_keys('Frank')
-        last_name = driver.find_element(By.CSS_SELECTOR, '.first_block div:nth-child(2) input')
+        last_name = driver.find_element(By.CSS_SELECTOR, '.first_block .second_class input')
         last_name.send_keys('Martin')
-        email = driver.find_element(By.CSS_SELECTOR, '.first_block div:nth-child(3) input')
+        email = driver.find_element(By.CSS_SELECTOR, '.first_block .third_class input')
         email.send_keys('frank@omg.com')
 
         # submit form
@@ -36,9 +36,40 @@ class TestRegistration(unittest.TestCase):
         self.assertEqual(welcome_text, expected_welcome_message, "Welcome message is not equal to expected message")
 
         # pause to check result
-        time.sleep(10)
+        time.sleep(5)
         # quit browser
         driver.quit()
+
+    def test_registration2_negative(self):
+        driver = webdriver.Chrome(service=webdriver.chrome.service.Service(
+            executable_path='C:\projects\Tools\chrome-driver\chromedriver.exe'))
+        driver.implicitly_wait(5)
+
+        link = "http://suninjuly.github.io/registration2.html"
+        driver.get(link)
+
+        # fill in obligatory fields - no suchElementEx last_name
+        first_name = driver.find_element(By.CSS_SELECTOR, '.first_block .first_class input')
+        first_name.send_keys('Frank')
+        last_name = driver.find_element(By.CSS_SELECTOR, '.first_block .second_class input')
+        last_name.send_keys('Martin')
+        email = driver.find_element(By.CSS_SELECTOR, '.first_block .third_class input')
+        email.send_keys('frank@omg.com')
+
+        # submit form
+        submit = driver.find_element(By.CSS_SELECTOR, '.btn')
+        submit.click()
+
+        # find element with welcome text and get text
+        welcome_text = driver.find_element(By.TAG_NAME, "h1").text
+
+        # check if registration was successful
+        expected_welcome_message = "Congratulations! You have successfully registered!"
+        self.assertEqual(welcome_text, expected_welcome_message, "Welcome message is not equal to expected message")
+
+        # quit browser
+        driver.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
